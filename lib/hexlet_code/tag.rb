3e794176f6361e3daa282595ceead3b55dd805e7
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2022-07-19 17:05:22 +0500

    Fixed lint

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -7,12 +7,13 @@ module HexletCode
     def self.build(tag, attrs = {})
       content = block_given? ? yield : ''
       return "<#{tag}#{get_attr(attrs)}>#{content}</#{tag}>" if paired?(tag)
+
       "<#{tag}#{get_attr(attrs)}>"
     end
 
     def self.paired?(tag)
       single_tags = %w[input br img]
-      not single_tags.include? tag
+      !single_tags.include? tag
     end
 
     def self.get_attr(options_part)
